2a17e4224b9fff0425152b16860f5c085bf7dac6
sansjo <susanna.sjoberg@ahlens.se>
2025-06-02 08:33:00 +0200

    Refactor auth middleware to use ES module syntax

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-module.exports = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
         return res.status(401).json({ error: 'Unauthorized' });
@@ -13,3 +13,5 @@ module.exports = (req, res, next) => {
         return res.status(401).json({ error: 'Unauthorized' });
     }
 };
+
+export default authMiddleware;
