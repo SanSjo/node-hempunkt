dda8cce9eed948b22a55cae6c54457042e1b37e7
sansjo <susanna.sjoberg@ahlens.se>
2025-06-11 15:28:03 +0200

    Refactor CORS configuration to simplify middleware usage

--- server.js
+++ server.js
@@ -20,14 +20,7 @@ app.options('*', (req, res) => {
 });
 
 // Enhanced CORS configuration
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-}));
+app.use(cors());
 
 // Middleware to add CORS headers directly to all responses
 app.use((req, res, next) => {
