a8a1872502a49e557335aba0fed1aec15f219480
sansjo <susanna.sjoberg@ahlens.se>
2025-06-12 08:17:04 +0200

    refactor: update port configuration to use environment variable or default to 5000

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/', (req, res) => {
 });
 
 // Port configuration - using 3000
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log(`MongoDB connected successfully`);
